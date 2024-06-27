4c41c563afc352c7e7870bf09536d06bf7585cd8
umerfaisal <109797391+umerfaisal@users.noreply.github.com>
2024-06-27 18:07:41 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// Making the array of programming languages
+var programmingLanguages = ["Typescript", "Javascript ", "Python", "PHP"];
+//print the message of the list
+console.log("List of programming languages");
+//Printing the values of array in the form of list
+programmingLanguages.forEach(function (language) { return console.log(language); });
---
+++ main.ts
@@ -0,0 +1,8 @@
+// Making the array of programming languages
+let programmingLanguages = ["Typescript","Javascript ","Python","PHP"];
+
+//print the message of the list
+console.log("List of programming languages");
+
+//Printing the values of array in the form of list
+programmingLanguages.forEach(language => console.log(language));
